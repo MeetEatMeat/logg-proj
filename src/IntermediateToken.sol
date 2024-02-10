37d93c3a6b88547ce15b4038397e63827c46a8aa
DYaroslav <34601776+MeetEatMeat@users.noreply.github.com>
2024-02-10 14:34:13 +0500

    Comments added

--- src/IntermediateToken.sol
+++ src/IntermediateToken.sol
@@ -28,8 +28,7 @@ interface IBEP20 {
 }
 
 /// @title An intermediate fundrising token
-/// @author MeetEatMeat
-/// @notice This token will be changed to a permanent LOGG token as 1:1
+/// @notice LOGGI holders will be allowed to change their LOGGI tokens to LOGG token as 1:1
 contract LOGGI is ERC20, Ownable {
 
     event Minted(address, uint);
@@ -43,61 +42,62 @@ contract LOGGI is ERC20, Ownable {
     uint256 private _price = 5000000000000000 wei;// 0,005 USDT
     uint256 private _totalSaleAmount;
     address public constant _USDT = 0x55d398326f99059fF775485246999027B3197955;
+    address public constant _OWNER = ;
 
     error SaleNotActive();
     error ToLowAmount();
     error IncorrectAmount();
     error IncorrectAddress();
-    error IncorrectCount();
     error NotEnoughTotalSaleAmount();
     error NotWhitelisted();
+    error IncorrectPrice();
+    error AlreadyWhitelisted();
 
-    constructor(uint256 totalAmount) ERC20("Logarithm Games Intermediate Token", "LOGGI") Ownable(msg.sender){
+    constructor(uint256 totalAmount) ERC20("Logarithm Games Intermediate Token", "LOGGI") Ownable(_OWNER){
         _totalSaleAmount = totalAmount;
     }
 
-    function mint(uint amount, address to) public onlyOwner {
-        if (amount == 0) revert IncorrectAmount();
-        if (to == address(this)) revert IncorrectAddress();
-
-        _mint(to, amount);
-        emit Minted(to, amount);
-    }
-
     /// @notice This function is for public sale buyers
+    /// @param amount Amount is amount of USDT that buyer supposes to spend
+    /// @return Return the bought amount of LOGGI tokens
     function buy(uint256 amount) external returns(uint256){
         if (!publicSaleStatus) revert SaleNotActive();
 
         return _buy(amount);
     }
 
+    /// @notice This function is for whitelisted buyers
+    /// @param amount Amount is amount of USDT that buyer supposes to spend
+    /// @return Return the bought amount of LOGGI tokens
     function buyWl(uint256 amount) external returns(uint256){
         if (!whitelist[_msgSender()]) revert NotWhitelisted();
 
         return _buy(amount);
     }
 
-    function _buy(uint256 amount) internal returns(uint256){
-        if (amount < 1000000000000000000) revert ToLowAmount();// Should be at least a dollar
+    function _buy(uint256 usdtAmount) internal returns(uint256){
+        uint256 prevBalance = IBEP20(_USDT).balanceOf(address(this));
+        IBEP20(_USDT).transferFrom(msg.sender, address(this), usdtAmount);
+        uint256 newBalance = IBEP20(_USDT).balanceOf(address(this));
 
-        uint256 count = amount / _price;
-        if (count > _totalSaleAmount) revert NotEnoughTotalSaleAmount();
-        if (!(count > 0)) revert IncorrectCount();
+        if(!(newBalance > prevBalance)) revert ToLowAmount();
+        uint256 exactUsdtSpent = newBalance - prevBalance;
+        if (exactUsdtSpent < 1000000000000000000) revert ToLowAmount();// Should be at least a dollar
 
-        IBEP20(_USDT).transferFrom(msg.sender, address(this), amount);
+        if(_price == 0) revert IncorrectPrice();
+        uint256 loggiAmount = exactUsdtSpent / _price;
 
-        _totalSaleAmount -= count;
-        _mint(_msgSender(), count);
+        if (loggiAmount > _totalSaleAmount) revert NotEnoughTotalSaleAmount();
+        if (!(loggiAmount > 0)) revert IncorrectAmount();
 
-        emit Bought(_msgSender(), count);
-        return count;
+        _totalSaleAmount -= loggiAmount;
+        _mint(_msgSender(), loggiAmount);
+
+        emit Bought(_msgSender(), loggiAmount);
+        return loggiAmount;
     }
 
-    function burn(address account, uint256 amount) external onlyOwner {
-        _burn(account, amount);
-        emit Burned(account, amount);
-    }
-
+    /// @dev In BSC chain the USDT token has 18 digits so we have to use 18 digits with LOGGI token
     function decimals() public view virtual override returns (uint8) {
         return 18;
     }
@@ -106,41 +106,69 @@ contract LOGGI is ERC20, Ownable {
         return _price;
     }
 
-    function setPrice(uint256 price) external onlyOwner {
-        _price = price;
-    }
-
     function getSaleAmount() external view returns (uint256){
         return _totalSaleAmount;
-    }
-
-    function setSaleAmount(uint256 count) external onlyOwner {
-        _totalSaleAmount = count;
-    }
-
-    function setSaleStatePublic(bool newSaleState) external onlyOwner {
-        publicSaleStatus = newSaleState;
-    }
-
-    function addToWhitelist(address _user) external onlyOwner {
-        whitelist[_user] = true;
-    }
-
-    function addToWhitelistBatch(address[] memory _users) external onlyOwner {
-        for (uint256 i = 0; i < _users.length; i++) {
-            whitelist[_users[i]] = true;
-        }
-    }
-
-    function removeFromWhitelist(address _user) external onlyOwner {
-        whitelist[_user] = false;
     }
 
     function isWhitelisted(address _user) external view returns (bool){
         return whitelist[_user];
     }
 
+    ////////////////////// OWNER'S FUNCTIONS ///////////////////////
+
+    /// @notice This function is for minting tokens
+    /// @param amount Amount of tokens to mint
+    function mint(uint amount, address to) public onlyOwner {
+        if (amount == 0) revert IncorrectAmount();
+        if (to == address(this)) revert IncorrectAddress();
+
+        _mint(to, amount);
+        emit Minted(to, amount);
+    }
+
+    function setPrice(uint256 price) external onlyOwner {
+        if(price == 0) revert IncorrectPrice();
+        _price = price;
+    }
+
+    function setSaleAmount(uint256 amount) external onlyOwner {
+        if(amount == 0) revert IncorrectAmount();
+        _totalSaleAmount = amount;
+    }
+
+    /// @notice This function close or open public sale
+    /// @param newSaleState If this param is true sale is opened and otherwise sale is closed
+    function setSaleStatePublic(bool newSaleState) external onlyOwner {
+        publicSaleStatus = newSaleState;
+    }
+
+    /// @notice A function to add a single user into whitelist
+    function addToWhitelist(address _user) external onlyOwner {
+        _addToWhitelist(_user);
+    }
+
+    /// @notice A function to add a batch of users into whitelist
+    /// @param _users An array of adresses to add into whitelist
+    function addToWhitelistBatch(address[] memory _users) external onlyOwner {
+        for (uint256 i = 0; i < _users.length; i++) {
+            _addToWhitelist(_users[i]);
+        }
+    }
+
+    function _addToWhitelist(address _user) internal {
+        if(whitelist[_user]) revert AlreadyWhitelisted();
+        whitelist[_user] = true;
+        emit AddedToWhitelist(_user);
+    }
+
+    function removeFromWhitelist(address _user) external onlyOwner {
+        whitelist[_user] = false;
+        emit RemovedFromWhitelist(_user);
+    }
+
     function withdraw() external onlyOwner {
-        IBEP20(_USDT).transfer(owner(),  IBEP20(_USDT).balanceOf(address(this)));
+        uint256 amountToWithdraw = IBEP20(_USDT).balanceOf(address(this));
+        require(amountToWithdraw > 0, "Nothing to withdraw");
+        IBEP20(_USDT).transfer(owner(),  amountToWithdraw);
     }
 }
