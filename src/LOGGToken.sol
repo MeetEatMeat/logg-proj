ebd28ea97d72a1ffb67649db8bf7293b99f669a1
DYaroslav <34601776+MeetEatMeat@users.noreply.github.com>
2024-02-13 12:00:06 +0500

    Structure changes, burn() added, WL removed

--- src/LOGGToken.sol
+++ src/LOGGToken.sol
@@ -1,17 +1,169 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract LOGGToken is ERC20, AccessControl {
-    using SafeERC20 for IERC20;
+interface IBEP20 {
+  function totalSupply() external view returns (uint256);
 
-    constructor(address privateSaleContract, address loggVault) ERC20("Logarithm Games", "LOGG"){
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _mint()
+  function decimals() external view returns (uint8);
 
+  function symbol() external view returns (string memory);
+
+  function name() external view returns (string memory);
+
+  function getOwner() external view returns (address);
+
+  function balanceOf(address account) external view returns (uint256);
+
+  function transfer(address recipient, uint256 amount) external returns (bool);
+
+  function allowance(address _owner, address spender) external view returns (uint256);
+
+  function approve(address spender, uint256 amount) external returns (bool);
+
+  function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+}
+
+/// @title A Logarithm Games project BSC network token
+
+contract LOGG is ERC20, Ownable {
+
+    event Minted(address, uint);
+    event Bought(address, uint);
+    event Burned(address, uint);
+
+    bool private _saleStatus;
+    uint256 private _price = 5000000000000000 wei;// 0,005 USDT
+    uint256 private _totalSaleAmount;
+    address public constant _USDT = 0x55d398326f99059fF775485246999027B3197955;
+    uint256 private constant MAX_TOTAL_SUPPLY = 1_000_000_000e18;
+
+    error SaleNotActive();
+    error ToLowAmount();
+    error IncorrectAmount();
+    error IncorrectAddress();
+    error ExceededTotalSaleAmount();
+    error IncorrectPrice();
+
+    constructor(uint256 totalAmount, address owner) ERC20("Logarithm Games Token", "LOGG") Ownable(owner){
+        _totalSaleAmount = totalAmount;
     }
 
+    //////////////////////  USER'S FUNCTIONS  ///////////////////////
+
+    /// @notice This function is for public sale buyers
+    /// @param amount Amount is amount of USDT(BSC) that buyer supposes to spend
+    /// @return Return the bought amount of LOGG tokens
+    function buy(uint256 amount) external returns(uint256){
+        if (!_saleStatus) revert SaleNotActive();
+        if ((amount + totalSupply()) > MAX_TOTAL_SUPPLY) revert IncorrectAmount();
+        if (amount > _totalSaleAmount) revert ExceededTotalSaleAmount();
+        if (amount == 0) revert IncorrectAmount();
+
+        return _buy(amount);
+    }
+
+    //////////////////////  GETTERS  ///////////////////////
+
+    /// @dev In BSC chain the USDT token has 18 digits so we have to use 18 digits with LOGGI token
+    function decimals() public view virtual override returns (uint8) {
+        return 18;
+    }
+
+    function getPrice() external view returns (uint256){
+        return _price;
+    }
+
+    function getSaleTotalAmount() external view returns (uint256){
+        return _totalSaleAmount;
+    }
+
+    function getSaleStatus() external view returns (bool){
+        return _saleStatus;
+    }
+
+    ////////////////////// OWNER'S FUNCTIONS ///////////////////////
+
+    /// @notice This function is for minting tokens
+    /// @param amount Amount of tokens to mint
+    function mint(uint256 amount, address to) external onlyOwner {
+        if (amount == 0) revert IncorrectAmount();
+        if ((amount + totalSupply()) < totalSupply()) revert IncorrectAmount();
+        if ((amount + totalSupply()) > MAX_TOTAL_SUPPLY) revert IncorrectAmount();
+        if (to == address(this)) revert IncorrectAddress();
+
+        _mint(to, amount);
+        emit Minted(to, amount);
+    }
+
+    function burn(uint256 amount, address from) external onlyOwner {
+        if (amount == 0) revert IncorrectAmount();
+        if (from == address(this)) revert IncorrectAddress();
+
+        _burn(from, amount);
+        emit Burned(from, amount);
+    }
+
+    function setPrice(uint256 price) external onlyOwner {
+        if(price == 0) revert IncorrectPrice();
+        _price = price;
+    }
+
+    function setSaleTotalAmount(uint256 amount) external onlyOwner {
+        if(amount == 0) revert IncorrectAmount();
+        _totalSaleAmount = amount;
+    }
+
+    /// @notice This function close or open public sale
+    /// @param newSaleStatus If this param is true sale is opened and otherwise sale is closed
+    function setSaleStatus(bool newSaleStatus) external onlyOwner {
+        _saleStatus = newSaleStatus;
+    }
+
+    /// @notice Since this token is a fundrising token all value will be received by team
+    function withdrawAll() external onlyOwner {
+        uint256 amountToWithdraw = IBEP20(_USDT).balanceOf(address(this));
+        require(amountToWithdraw > 0, "Nothing to withdraw");
+        IBEP20(_USDT).transfer(owner(),  amountToWithdraw);
+    }
+
+    function withdraw(uint256 amount) external onlyOwner {
+        uint256 balance = IBEP20(_USDT).balanceOf(address(this));
+        require(balance >= amount, "Nothing to withdraw");
+        IBEP20(_USDT).transfer(owner(), amount);
+    }
+
+    ////////////////////// INTERNAL FUNCTIONS ///////////////////////
+
+    function _buy(uint256 amount) internal returns(uint256){
+        uint256 loggAmount = amount;
+        if(_price == 0) revert IncorrectPrice();
+        uint256 usdtAmount = loggAmount * _price;// Safe since 0.8 solc
+
+        uint256 prevBalance = IBEP20(_USDT).balanceOf(address(this));
+        IBEP20(_USDT).transferFrom(msg.sender, address(this), usdtAmount);
+        uint256 newBalance = IBEP20(_USDT).balanceOf(address(this));
+
+        if(!(newBalance > prevBalance)) revert ToLowAmount();
+        uint256 exactUsdtSpent = newBalance - prevBalance;
+        if (exactUsdtSpent < 1000000000000000000) revert ToLowAmount();// Should be at least a dollar
+        
+        if (exactUsdtSpent < usdtAmount){
+            uint256 exactLoggAmount = exactUsdtSpent / _price;
+            _totalSaleAmount -= exactLoggAmount;
+            _mint(_msgSender(), exactLoggAmount);
+            
+            emit Bought(_msgSender(), exactLoggAmount);
+            return exactLoggAmount;
+        }
+
+        _totalSaleAmount -= loggAmount;
+        _mint(_msgSender(), loggAmount);
+
+        emit Bought(_msgSender(), loggAmount);
+        return loggAmount;
+    }
 }
---
+++ test/LOGGToken.t.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "forge-std/Test.sol";
+import "../src/LOGGToken.sol";
+
+contract LOGGTest is Test {
+    LOGG public _logg;
+    address _owner;
+    uint256 _totalSaleAmount;
+    address _wluser1;
+    address _wluser2;
+    address _wluser3;
+    address _wluser4;
+    address _psuser;
+    address[] users;
+
+    function setUp() public {
+        _totalSaleAmount = 1000000e18;
+        _owner = vm.addr(0x0001);
+        _wluser1 = vm.addr(0x0002);
+        _wluser2 = vm.addr(0x0003);
+        _wluser3 = vm.addr(0x0004);
+        _wluser4 = vm.addr(0x0005);
+        _psuser = vm.addr(0x0006);
+        _logg = new LOGG(_totalSaleAmount, _owner);
+        vm.prank(_owner);
+        _logg.setSaleStatus(true);
+    }
+
+    function test_Getters() public {
+        assertEq(_logg.getSaleTotalAmount(), _totalSaleAmount);
+        assertEq(_logg.decimals(), 18);
+        assertEq(_logg.getPrice(), 5e15);
+        assertEq(_logg.getSaleStatus(), true);
+    }
+
+    function test_setSaleTotalAmount() public {
+        uint256 newSaleAmount = _totalSaleAmount * 2;
+        vm.prank(_owner);
+        _logg.setSaleTotalAmount(newSaleAmount);
+        assertEq(_logg.getSaleTotalAmount(), newSaleAmount);
+    }
+
+    function test_setPrice() public {
+        vm.prank(_owner);
+        _logg.setPrice(6e15);
+        assertEq(_logg.getPrice(), 6e15);
+    }
+
+    function test_Mint() public {
+        vm.prank(_owner);
+        _logg.mint(100e18, _wluser4);
+        assertEq(_logg.balanceOf(_wluser4), 100e18);
+    }
+}
